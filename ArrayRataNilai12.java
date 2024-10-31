84d208d18a9461183b8cdbb112b3cb9353349f11
ocaocuoci <hernandar0606@gmail.com>
2024-10-31 22:21:42 +0700

    tampilkan rata rata lulus dan tidak lulus serta input jumlah mahasiswa

--- ArrayRataNilai12.java
+++ ArrayRataNilai12.java
@@ -4,23 +4,34 @@ public class ArrayRataNilai12{
 
         Scanner input =new Scanner(System.in);
 
-        double rata2;
-        int [] nilaiMhs =new int[10];
+        double rata2Lulus, rata2TdkLulus, jmlLulus =0, jmlTdkLulus = 0;
+       
         int total =0;
         int lulus =0;
+        int tidakLulus = 0;;
+        int jmlMhs;
+
+        System.out.print("Masukkan jumlah mahasiswa: ");
+        jmlMhs = input.nextInt();
+        
+         int [] nilaiMhs =new int[jmlMhs];
 
         for (int i = 0; i < nilaiMhs.length; i++){
             System.out.print("Masukkan nilai mahasiswa ke-"+(i+1)+ ": ");
             nilaiMhs[i] = input.nextInt();    
         }
         for (int i =0; i < nilaiMhs.length; i++){
-            total += nilaiMhs[i];
             if (nilaiMhs[i] > 70){
+                jmlLulus += nilaiMhs[i];
                 lulus++;
+            } else {
+                jmlTdkLulus += nilaiMhs[i];
+                tidakLulus++;
             }
-        }
-        rata2 = total/nilaiMhs.length;
-        System.out.println("Rata - rata nilai = " +rata2);
-        System.out.println("Jumlah mahasiwa yang lulus adalah " + lulus);
+        } 
+        rata2Lulus = (lulus > 0) ? jmlLulus / lulus : 0;
+        rata2TdkLulus = (tidakLulus > 0) ? jmlTdkLulus / tidakLulus :0;
+        System.out.println("Rata - rata nilai lulus= " +rata2Lulus);
+        System.out.println("Rata - rata nilai tidak lulus= " +rata2TdkLulus);
     }
 }
