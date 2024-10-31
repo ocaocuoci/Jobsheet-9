7e59273fba04e6512d78372210fb3fb77aa4de3d
ocaocuoci <hernandar0606@gmail.com>
2024-10-31 20:46:16 +0700

    pertanyaan no 4

--- ArrayBilangan12.java
+++ ArrayBilangan12.java
@@ -3,10 +3,9 @@ public class ArrayBilangan12{
 
         int [] bil ={5, 13, -7, 17};
 
-        System.out.println(bil[0]);
-        System.out.println(bil[1]);
-        System.out.println(bil[2]);
-        System.out.println(bil[3]);
+    for (int i = 0; i <= 4; i++){
+        System.out.println(bil[i]);
+    }
 
     }
 }
