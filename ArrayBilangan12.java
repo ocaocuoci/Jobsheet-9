d56c888a1a86d2b828b1ad5302f29e9205882213
ocaocuoci <hernandar0606@gmail.com>
2024-10-31 20:29:37 +0700

    pertanyaan nomer 1

--- ArrayBilangan12.java
+++ ArrayBilangan12.java
@@ -1,12 +1,12 @@
 public class ArrayBilangan12{
     public static void  main (String[] args){
 
-        int [] bil =new int[4];
+        double [] bil =new double[4];
 
-        bil[0] = 5;
-        bil[1] = 13;
-        bil[2] = -7;
-        bil[3] = 17;
+        bil[0] = 5.0;
+        bil[1] = 12867;
+        bil[2] = 7.5;
+        bil[3] = 2000000;
 
         System.out.println(bil[0]);
         System.out.println(bil[1]);
