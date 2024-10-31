a584126c0789948996dbe478878c610657c109a7
ocaocuoci <hernandar0606@gmail.com>
2024-10-31 21:56:01 +0700

    jumlah lulus > 70

--- ArrayRataNilai12.java
+++ ArrayRataNilai12.java
@@ -4,19 +4,23 @@ public class ArrayRataNilai12{
 
         Scanner input =new Scanner(System.in);
 
+        double rata2;
         int [] nilaiMhs =new int[10];
         int total =0;
-        double rata2;
+        int lulus =0;
 
         for (int i = 0; i < nilaiMhs.length; i++){
             System.out.print("Masukkan nilai mahasiswa ke-"+(i+1)+ ": ");
-            nilaiMhs[i] = input.nextInt();
-
-        } 
+            nilaiMhs[i] = input.nextInt();    
+        }
         for (int i =0; i < nilaiMhs.length; i++){
             total += nilaiMhs[i];
+            if (nilaiMhs[i] > 70){
+                lulus++;
+            }
         }
         rata2 = total/nilaiMhs.length;
         System.out.println("Rata - rata nilai = " +rata2);
+        System.out.println("Jumlah mahasiwa yang lulus adalah " + lulus);
     }
 }
