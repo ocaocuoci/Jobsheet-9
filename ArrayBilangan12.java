1685a92d4803266a5ca8e17ba64fed25a32ffafd
ocaocuoci <hernandar0606@gmail.com>
2024-10-31 20:33:51 +0700

    pertanyaan 2, inisialisasi array

--- ArrayBilangan12.java
+++ ArrayBilangan12.java
@@ -1,12 +1,7 @@
 public class ArrayBilangan12{
     public static void  main (String[] args){
 
-        double [] bil =new double[4];
-
-        bil[0] = 5.0;
-        bil[1] = 12867;
-        bil[2] = 7.5;
-        bil[3] = 2000000;
+        int [] bil ={5, 13, -7, 17};
 
         System.out.println(bil[0]);
         System.out.println(bil[1]);
