a9b32bc552d08acfe9901e96d0dc1fff087173f5
ocaocuoci <hernandar0606@gmail.com>
2024-11-01 07:16:06 +0700

    input banyak nilai, input key

--- SearchNilai12.java
+++ SearchNilai12.java
@@ -1,18 +1,33 @@
+import java.util.Scanner;
 public class SearchNilai12{
     public static void main (String[] args) {
+        Scanner input =new Scanner(System.in);
 
 
-        int [] ArrayNilai = {80, 85, 78, 96, 90, 82, 86};
-        int key = 90;
-        int hasil =0;
+        int key = 0;
+        int hasil =-1;
+        int nilai;
+
+        System.out.print("Masukkan banyaknya nilai yang akan diinput: ");
+        nilai = input.nextInt();
+
+        int ArrayNilai []= new int [nilai];
 
         for (int i = 0; i < ArrayNilai.length; i++){
-            if (key == ArrayNilai[i]){
-                hasil = i;
-                break;
-            }
-
+            System.out.print("Masukkan nilai mahasiswa ke-" +(i + 1)+": ");
+            ArrayNilai[i] = input.nextInt();
         }
+
+        System.out.print("Masukkan nilai yang ingin dicari: ");
+        key = input.nextInt();
+        
+        for (int i =0; i < ArrayNilai.length; i++){
+             if (key == ArrayNilai[i]){
+                hasil = (i+1);
+                break;
+            } 
+        }
+          
         System.out.println();
         System.out.println("Nilai " +key + " Berada di indeks ke-" +hasil);
         System.out.println();
